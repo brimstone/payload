9872a8fe7cdfe9c061d8006824da48670f23c033
Matt Robinson <brimstone@the.narro.ws>
2016-12-11 21:50:37 -0500

    Fix non-terminated array

--- action_local_shell.c
+++ action_local_shell.c
@@ -3,7 +3,7 @@
 #include "actions.h"
 
 int action_local_shell(){
-	execv("/bin/sh", (char *[]){"/bin/sh"});
+	execv("/bin/sh", (char *[]){"/bin/sh", 0});
 	return 1;
 }
 void action_local_shell_help(){
